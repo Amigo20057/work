8333e067de7d6f2b1ecabe14be0e70fdc692b508
Amigo20057 <denrizuk@gmail.com>
2025-01-26 09:35:32 +0200

    fix sql tables

--- server/src/db/tables.sql
+++ server/src/db/tables.sql
@@ -1,24 +1,24 @@
 -- Таблица пользователей
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY, -- Уникальный идентификатор пользователя
-    user_name VARCHAR(255) NOT NULL, -- Полное имя пользователя
+    id SERIAL PRIMARY KEY, 
+    user_name VARCHAR(255) NOT NULL, 
     user_surname VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE, -- Email (уникальный)
-    password VARCHAR(255) NOT NULL, -- Хэш пароля
-    roles VARCHAR(255) NOT NULL, -- Роли пользователя
-    name_company VARCHAR(255) UNIQUE, -- Название компании (уникальное, если применимо)
-    address_company VARCHAR(255) UNIQUE, -- Адрес компании (уникальное, если применимо)
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата создания записи
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Дата обновления записи
+    email VARCHAR(255) NOT NULL UNIQUE, 
+    password VARCHAR(255) NOT NULL, 
+    roles VARCHAR(255) NOT NULL, 
+    name_company VARCHAR(255) UNIQUE,
+    address_company VARCHAR(255) UNIQUE, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
 );
 
 -- Таблица постов
 CREATE TABLE posts (
-    id SERIAL PRIMARY KEY, -- Уникальный идентификатор поста
-    title VARCHAR(255) NOT NULL, -- Название поста
-    description TEXT NOT NULL, -- Описание поста
-    tags VARCHAR(255), -- Теги поста (возможно, через запятую)
-    recruiter_id INT REFERENCES users(id) ON DELETE CASCADE, -- Внешний ключ на таблицу users
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата создания записи
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Дата обновления записи
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL, 
+    description TEXT NOT NULL, 
+    tags VARCHAR(255), 
+    recruiter_id INT REFERENCES users(id) ON DELETE CASCADE, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
 );
